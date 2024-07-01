00e91302e199ace4f51776a4637fd54eb01633af
lmoran5 <zonaluke25@gmail.com>
2024-07-01 11:43:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -120,4 +120,3 @@ document.getElementById('subSpecialty').addEventListener('change', () => filterB
 document.getElementById('productDropdown').addEventListener('change', () => filterByDropdown('productDropdown', 5));
 document.getElementById('authorSearch').addEventListener('input', () => searchByField('authorSearch', 6));
 
-
